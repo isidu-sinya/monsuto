f801df6c4b1064b473be6847fd15cb82d7407dda
ishidu-shinya <shinya.3268@gmail.com>
2020-01-21 15:24:17 +0900

    title modified

--- single.php
+++ single.php
@@ -6,30 +6,19 @@
           if (have_posts() ) :
               while (have_posts() ) : the_post();
               ?>
-            <article id="post-1" class="entry">
+            <article id="post-<?php the_ID(); ?>" <?php post_class('entry'); ?>>
                 <h1 class="type-A">新着情報</h1>
-                <h2 class="title type-B"><span>お花見企画の予約を開始しました</span></h2>
+                <h2 class="title type-B"><span><?php the_title(); ?></span></h2>
 
                 <div class="entryInfo">
                     <div class="categories">
-                        <ul>
-                            <li>お知らせ</li>
-                        </ul>
+                        <?php the_category(); ?>
                     </div>
-                    <time datetime="2015-03-22">2015.03.22(sun)</time>
+                    <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y年m月d日(l)'); ?></time>
                 </div><!-- /.entryInfo -->
 
                 <section class="content">
-                    <h1>今年も恒例のお花見企画を行います。</h1>
-                    <img src="images/dummy/670x260.png" height="260" width="670" alt="">
-                    <p>当ホテルの中庭にてお花見をしませんか？<br>毎年、多くのお客さまにご好評を頂いております。</p>
-                    <h2>企画詳細</h2>
-                    <p>日付：4月11日（土）</p>
-                    <p>時間：13:00〜17:00</p>
-                    <p>場所：当ホテル - 中庭にて</p>
-                    <p>費用：1名様 3500円（12才以下は1500円）</p>
-                    <h3>お料理について</h3>
-                    <p>料理はバイキング形式で和洋折衷でご提供します。<br>お飲物もアルコールからジュースまで飲み放題でご提供します。</p>
+                  <?php the_content(); ?>
                 </section><!-- /.content -->
 
                 <nav class="postNavi">
