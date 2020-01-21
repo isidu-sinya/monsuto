86fad567caa5dab2a8e1b79e1ee7089f4c2ebd22
ishidu-shinya <shinya.3268@gmail.com>
2020-01-21 20:36:14 +0900

    title modified

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                         <p>[<a href="<?php the_permalink(); ?>">続きを読む</a>]</p>
                     </div>
                     <figure>
-                      <?php if (has_post_thumbnail() ): ?>
+                      <?php if ( has_post_thumbnail() ): ?>
                       <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
                     <?php else: ?>
                         <a href="<?php the_permalink(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/common/noimage_180x180.png" height="180" width="180" alt=""></a>
