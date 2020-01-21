eb07edfb3603048792e2dfa19a7ce08234eded82
ishidu-shinya <shinya.3268@gmail.com>
2020-01-21 14:12:59 +0900

    title modified

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                         <?php the_experpt(); ?>
                         <p>[<a href="<?php the_permalink(); ?>">続きを読む</a>]</p>
                     </div>
-                    <figure><a href="single.html"><img src="<?php echo get_template_directory_uri(); ?>/images/dummy/180x180-3.png" height="180" width="180" alt=""></a></figure>
+                    <figure><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a></figure>
                 </article><!-- /.news -->
                 <?php
                     endwhile;
