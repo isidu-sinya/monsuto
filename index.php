8bacce4c5e47bcc296e4d7b3c0e9b447d98096f1
ishidu-shinya <shinya.3268@gmail.com>
2020-01-21 13:02:07 +0900

    title modified

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
             <section class="newsBlock block">
                 <h1 class="type-B"><span>新着情報</span></h1>
 
+                <?php
+                if ( have_posts() ):
+                     while (have_posts() ): the_post(); ?>
                 <article class="news">
                     <div class="text">
                         <div class="entryInfo">
@@ -33,46 +36,9 @@
                     </div>
                     <figure><a href="single.html"><img src="<?php echo get_template_directory_uri(); ?>/images/dummy/180x180-3.png" height="180" width="180" alt=""></a></figure>
                 </article><!-- /.news -->
-
-                <article class="news">
-                    <div class="text">
-                        <div class="entryInfo">
-                            <div class="categories">
-                                <ul>
-                                    <li><a href="#">コラム</a></li>
-                                </ul>
-                            </div>
-                            <time datetime="2015-02-14">2015.02.14(sat)</time>
-                        </div>
-                        <h1><a href="single.html">バレンタイン企画を行います</a></h1>
-                        <p>
-                            本日はバレンタインデーですね。当ホテルでもバレンタイン企画を行います。
-                            ディナーをご予約されたお客さま限定で特別コースをご提供させて頂きます。
-                        </p>
-                        <p>[<a href="single.html">続きを読む</a>]</p>
-                    </div>
-                    <figure><a href="single.html"><img src="<?php echo get_template_directory_uri(); ?>/images/dummy/180x180-2.png" height="180" width="180" alt=""></a></figure>
-                </article><!-- /.news -->
-
-                <article class="news">
-                    <div class="text">
-                        <div class="entryInfo">
-                            <div class="categories">
-                                <ul>
-                                    <li><a href="#">お知らせ</a></li>
-                                </ul>
-                            </div>
-                            <time datetime="2015-01-02">2015.01.02(fri)</time>
-                        </div>
-                        <h1><a href="single.html">今年も宜しくお願いいたします</a></h1>
-                        <p>
-                            新年明けましておめでとうございます。今年もホテル・技評リゾート石垣島を何卒よろしくお願いいたします。
-                            スタッフ一同、心よりお待ちしております。
-                        </p>
-                        <p>[<a href="single.html">続きを読む</a>]</p>
-                    </div>
-                    <figure><a href="single.html"><img src="<?php echo get_template_directory_uri(); ?>/images/dummy/180x180-1.png" height="180" width="180" alt=""></a></figure>
-                </article><!-- /.news -->
+                <?php
+                    endwhile;
+                  endif; ?>
 
             </section><!-- /.newsBlock -->
 
