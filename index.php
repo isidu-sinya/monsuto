af1551cb97c173d203fb74ffce9cc2676d3e3ac2
ishidu-shinya <shinya.3268@gmail.com>
2020-01-25 12:55:11 +0900

    title modified

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
                 <?php
                 if ( have_posts() ):
-                     while (have_posts() ): the_post(); ?>
+                   while ( have_posts() ): the_post(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class('news'); ?>>
                     <div class="text">
                         <div class="entryInfo">
