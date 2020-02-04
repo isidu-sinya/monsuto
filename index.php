43179fdedca77f2df2dddb300d91daec327cacdf
ishidu-shinya <shinya.3268@gmail.com>
2020-02-04 17:00:29 +0900

    title modified

--- index.php
+++ index.php
@@ -2,29 +2,13 @@
 
     <div class="contentsWrap">
         <div class="mainContents">
-            <h1 class="type-A"><?php wp_title(''); ?></h1>
+            <?php if (is_month() ): ?>
+              <h1 class="type-A"><?php the_time('Y年m月'); ?></h1>
+            <?php else: ?>
+              <h1 class="type-A"><?php wp_title(''); ?></h1>
+            <?php endif; ?>
             <section class="newsBlock block">
-
-                <article class="news">
-                    <div class="text">
-                        <div class="entryInfo">
-                            <div class="categories">
-                                <ul>
-                                    <li><a href="#">コラム</a></li>
-                                </ul>
-                            </div>
-                            <time datetime="2015-02-14">2015.02.14(sat)</time>
-                        </div>
-                        <h1><a href="single.html">バレンタイン企画を行います!!</a></h1>
-                        <p>
-                            本日はバレンタインデーですね。当ホテルでもバレンタイン企画を行います。
-                            ディナーをご予約されたお客さま限定で特別コースをご提供させて頂きます。
-                        </p>
-                        <p>[<a href="single.html">続きを読む</a>]</p>
-                    </div>
-                    <figure><a href="single.html"><img src="images/dummy/180x180-2.png" height="180" width="180" alt=""></a></figure>
-                </article><!-- /.news -->
-
+              <?php get_template_part('loop' ,'main'); ?>
             </section><!-- /.newsBlock -->
         </div><!-- /.mainContents -->
 
