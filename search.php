4ea9e6a0416b3131c339f1f5933a5ec0bf21632e
ishidu-shinya <shinya.3268@gmail.com>
2020-02-08 14:20:20 +0900

    title modified

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <div class="contentsWrap">
         <div class="mainContents">
-            <h1 class="type-A">「<?php the_search_query(); ?>」の検索結果</h1>
+            <h1 class="type-A">[<?php the_search_query(); ?>]の検索結果</h1>
             <section class="newsBlock block">
             <?php get_template_part( 'loop', 'main' ); ?>
             </section><!-- /.newsBlock -->
