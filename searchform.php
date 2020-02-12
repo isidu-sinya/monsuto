5df274f1a03591231c0d3f43fe690ba3e7c6a3cd
ishidu-shinya <shinya.3268@gmail.com>
2020-02-12 21:40:40 +0900

    title modified

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form action="<?php echo home_url('/'); ?>" method="get" class="searchform">
+<form action="<?php echo home_url('l'); ?>" method="get" class="searchform">
     <input type="text" value="<?php the_search_query(); ?>" name="s" id="s" placeholder="サイト内検索">
     <input type="submit" id="searchSubmit" value="">
 </form>
