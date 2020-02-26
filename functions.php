0e11ebbd14ca182565a781132e657b591208e4df
ishidu-shinya <shinya.3268@gmail.com>
2020-02-26 18:33:45 +0900

    title modified

--- functions.php
+++ functions.php
@@ -7,13 +7,3 @@ add_theme_support('post-thumbnails');
 *カスタムメニュー昨日を使用可能にする
 */
 add_theme_support( 'menus' );
-
-function mycustom_wp_footer() {
-?>
-<script type="text/javascript">
-document.addEventListener( 'wpcf7mailsent', function( event ) {
-    location = 'http://サイトドメイン/contact/thanks';
-}, false );
-</script>
-<?php
-}
