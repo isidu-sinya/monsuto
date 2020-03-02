6ef5d76c35eb24ad43fb5028476a7e132acb269e
ishidu-shinya <shinya.3268@gmail.com>
2020-03-02 18:20:26 +0900

    title modified

--- functions.php
+++ functions.php
@@ -7,3 +7,8 @@ add_theme_support('post-thumbnails');
 *カスタムメニュー昨日を使用可能にする
 */
 add_theme_support( 'menus' );
+add_filter('comment_form_default_fields', 'my_comment_form_default_fields');
+functions my_comment_form_default_fields( $args ) {
+  $args['url'] = ''; //「ウェブサイト」を削除
+  return $args;
+}
