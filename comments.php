743d6c7b90b49f477303f5f07197ea52359a7987
ishidu-shinya <shinya.3268@gmail.com>
2020-03-06 19:50:16 +0900

    title modified

--- comments.php
+++ comments.php
@@ -9,7 +9,12 @@ if ( have_comments() ) :
 ?>
     <p><?php comments_number(); ?></p>
     <ol class="commentlist">
-        <?php wp_list_comments( $wp_list_comments_args ); ?>
+        <?php
+        $wp_list_comments_args = array(
+          'avater_size' => '50'
+        );
+        wp_list_comments( $wp_list_comments_args );
+        ?>
     </ol>
 <?php
 paginate_comments_links();
--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@ add_theme_support( 'menus' );
 
 add_filter('comment_form_default_fields', 'my_comment_form_default_fields');
 function my_comment_form_default_fields( $args ) {
+  $args['author'] = ''; //「名前」を削除
+  $args['email'] = ''; //「メールアドレス」を削除
   $args['url'] = ''; //「ウェブサイト」を削除
   return $args;
 }
