2c701f07583b08ef053fa1390fffc8dcf43b34dd
ishidu-shinya <shinya.3268@gmail.com>
2020-03-13 18:28:29 +0900

    title modified

---
+++ template-1column.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+    <div class="contentsWrap">
+        <div class="mainContents oneColumn">
+            <article class="page">
+                <h1 class="type-A">ロングステイプラン</h1>
+
+                <section class="content">
+                    <p><img src="images/long-stay/pic01.png" height="300" width="1000" alt=""></p>
+                    <p class="aligncenter">
+                        石垣島で長期滞在をお考えの方、ウィークリーマンション<br><br>
+                        やマンスリーマンションをお探しの方におすすめのロングステイプラン。<br><br>
+                        キッチン付きなので、石垣島の新鮮素材で料理を楽しんだり、<br><br>
+                        無料のレンタサイクルで近くを観光したり。<br><br>
+                        石垣島の生活をゆっくり楽しんでみてはいかがでしょうか。
+                    </p>
+                </section>
+
+            </article><!-- /.page -->
+        </div><!-- /.mainContents -->
+    </div><!-- /.contentsWrap -->
+<?php get_footer(); ?>
